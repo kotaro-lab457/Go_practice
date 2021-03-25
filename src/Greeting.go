a538715f362fc548c2893b301e90d9236b871614
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-03-25 10:29:57 +0900

    first commit

---
+++ src/Greeting.go
@@ -0,0 +1,14 @@
+package main
+
+// フォーマットパッケージ
+import (
+	"fmt"
+)
+
+//  エントリーポイント
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println(("Good morning"))
+}
+
+// コンパイルするため、ビルド（build）を行う。「 go run Greeting.go 」

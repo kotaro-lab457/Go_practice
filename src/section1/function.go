bdcd22f3ae03d39d2ccdf57f887e139e8068e8b3
kotaro-lab457 <k.chanmpmp@icloud.com>
2022-09-15 00:06:19 +0900

    study

--- src/section1/function.go
+++ src/section1/function.go
@@ -7,8 +7,9 @@ import (
 
 func add(x int, y int) {
 	fmt.Println(x + y)
-	data := time.Now()
+	var data = time.Now().Day()
 	fmt.Println(data)
+
 }
 
 // returnを2つ
@@ -22,9 +23,12 @@ func cal(price, item int) (result int) {
 	return
 }
 
+const Pi = 3.14
+
 func main() {
 	add(10, 20)
 
+	// 型指定なし（暗黙的）
 	r1, r2 := add3(30, 40)
 	fmt.Println(r1, r2)
 	// 70 -10
@@ -32,4 +36,24 @@ func main() {
 	r3 := cal(10, 2)
 	fmt.Println(r3)
 	// 20
+
+	var a2 int64 = 500
+	var a3 int = 1
+	fmt.Printf("%T\n", int32(a3))
+	fmt.Println((a2 + int64(a3)))
+
+	var s string = "Hello"
+
+	fmt.Println(string(s[0]))
+
+	// var arr2 [15]string = [15]string{"A", "B"}
+	// fmt.Println(arr2)
+	var arr2 [2]string = [...]string{"A", "B"}
+	fmt.Println(arr2)
+
+	var arrB [2]int = [2]int{100, 900}
+
+	arrB[1] = 300
+
+	fmt.Println(arrB)
 }

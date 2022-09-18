3ab20288f11600f01c93d2ab43d2be289c12ed7f
kotaro-lab457 <k.chanmpmp@icloud.com>
2022-09-18 10:44:08 +0900

    learn

--- src/section1/function.go
+++ src/section1/function.go
@@ -18,7 +18,7 @@ func add3(x int, y int) (int, int) {
 }
 
 // 関数名を定義 result
-func cal(price, item int) (result int) {
+func cal(price int, item int) (result int) {
 	result = price * item
 	return
 }
@@ -29,31 +29,14 @@ func main() {
 	add(10, 20)
 
 	// 型指定なし（暗黙的）
-	r1, r2 := add3(30, 40)
-	fmt.Println(r1, r2)
+	r1, _ := add3(30, 40)
+	fmt.Println(r1)
 	// 70 -10
+
+	fmt.Printf("%T\n", r1)
 
 	r3 := cal(10, 2)
 	fmt.Println(r3)
 	// 20
 
-	var a2 int64 = 500
-	var a3 int = 1
-	fmt.Printf("%T\n", int32(a3))
-	fmt.Println((a2 + int64(a3)))
-
-	var s string = "Hello"
-
-	fmt.Println(string(s[0]))
-
-	// var arr2 [15]string = [15]string{"A", "B"}
-	// fmt.Println(arr2)
-	var arr2 [2]string = [...]string{"A", "B"}
-	fmt.Println(arr2)
-
-	var arrB [2]int = [2]int{100, 900}
-
-	arrB[1] = 300
-
-	fmt.Println(arrB)
 }
